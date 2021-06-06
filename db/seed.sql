04b6e738dab76589f10cc3f77d42cec745a0f9c4
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-05 20:55:41 -0700

    populate seed.sql with data

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,51 @@
 -- department seeds --
 INSERT INTO department (name)
-VALUE (" ");
+VALUE ('Content');
+INSERT INTO department (name)
+VALUE ('Design');
+INSERT INTO department (name)
+VALUE ('Development');
+INSERT INTO department (name)
+VALUE ('Release and Management');
 
 -- role seeds --
 INSERT INTO role (title, salary, department_id)
-VALUE (" ", , );
+VALUE ('Animator', 85000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Concept Artist', 95000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Game Designer', 90000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Level Designer', 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Story Designer', 70000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Game Developer', 110000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Project Manager', 120000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ('QA Engineer', 65000, 4);
 
 -- employee seeds --
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE (" ", " ",  , );
+VALUE ('Amy', 'Dixon', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Derrick', 'Barton', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Don', 'Harrington', 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Juanita', 'Barnett', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Tina', 'McKinney', 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Minnie', 'Manning', 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Irvin', 'Gray', 6, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Jacqueline', 'Burgess', 6, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Lindsey', 'Weaver', 7, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Armando', 'Griffith', 8, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Brett', 'Wong', 8, 4);
