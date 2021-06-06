5e070de33312a671fa38cf0e6c05fdcd54f89866
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-05 17:00:03 -0700

    create template for seed.sql

---
+++ seed.sql
@@ -0,0 +1,11 @@
+-- department seeds --
+INSERT INTO department (name)
+VALUE (" ");
+
+-- role seeds --
+INSERT INTO role (title, salary, department_id)
+VALUE (" ", , );
+
+-- employee seeds --
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE (" ", " ",  , );
