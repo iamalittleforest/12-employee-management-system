bfeee1d3e1fa3d7308c90d6117967d735d131a62
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-05 22:15:04 -0700

    add FOREIGN KEY to role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- create employee table --
@@ -23,5 +24,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
